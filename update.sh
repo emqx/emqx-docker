4d35ad3ac0d50502d9681eef2a134906e32cd5d8
Ivan Dyachkov <dev@dyachkov.org>
2024-01-12 15:02:20 +0100

    chore: 5.4.1
    also pin base docker image version by digest

--- update.sh
+++ update.sh
@@ -8,6 +8,9 @@ VERSION_DIR=$(echo "$VERSION" | cut -d '.' -f 1,2 -)
 AMD64_SHA256=$(curl -sSL https://github.com/emqx/emqx/releases/download/v${VERSION}/emqx-${VERSION}-debian11-amd64.tar.gz.sha256)
 ARM64_SHA256=$(curl -sSL https://github.com/emqx/emqx/releases/download/v${VERSION}/emqx-${VERSION}-debian11-arm64.tar.gz.sha256)
 
-sed -i'' -e "s/EMQX_VERSION=.*/EMQX_VERSION=${VERSION}/" $VERSION_DIR/Dockerfile
-sed -i'' -e "s/AMD64_SHA256=.*/AMD64_SHA256=${AMD64_SHA256}/" $VERSION_DIR/Dockerfile
-sed -i'' -e "s/ARM64_SHA256=.*/ARM64_SHA256=${ARM64_SHA256}/" $VERSION_DIR/Dockerfile
+sed -i.bak \
+    -e "s/EMQX_VERSION=.*/EMQX_VERSION=${VERSION}/" \
+    -e "s/AMD64_SHA256=.*/AMD64_SHA256=${AMD64_SHA256}/" \
+    -e "s/ARM64_SHA256=.*/ARM64_SHA256=${ARM64_SHA256}/" \
+    $VERSION_DIR/Dockerfile
+rm -f $VERSION_DIR/Dockerfile.bak
