3fe6e5dac6903e6a47a24f9b7acd6fd1449b33fd
Ivan Dyachkov <dev@dyachkov.org>
2025-09-04 11:01:21 +0200

    chore: emqx 5.8.8, switch to debian13-slim as base image

--- update.sh
+++ update.sh
@@ -5,8 +5,8 @@ set -eou pipefail
 VERSION="$1"
 VERSION_DIR=$(echo "$VERSION" | cut -d '.' -f 1,2 -)
 
-AMD64_SHA256=$(curl -sSL https://github.com/emqx/emqx/releases/download/v${VERSION}/emqx-${VERSION}-debian12-amd64.tar.gz.sha256)
-ARM64_SHA256=$(curl -sSL https://github.com/emqx/emqx/releases/download/v${VERSION}/emqx-${VERSION}-debian12-arm64.tar.gz.sha256)
+AMD64_SHA256=$(curl -sSL https://github.com/emqx/emqx/releases/download/v${VERSION}/emqx-${VERSION}-debian13-amd64.tar.gz.sha256)
+ARM64_SHA256=$(curl -sSL https://github.com/emqx/emqx/releases/download/v${VERSION}/emqx-${VERSION}-debian13-arm64.tar.gz.sha256)
 
 sed -i.bak \
     -e "s/EMQX_VERSION=.*/EMQX_VERSION=${VERSION}/" \
